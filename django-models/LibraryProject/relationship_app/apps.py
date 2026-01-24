8e7fcd819d7aba0706e4964e7b3780d91d2d7057
GebetaEst <estifanosgirma4@gmail.com>
2026-01-24 13:57:13 +0300

    c

--- django-models/LibraryProject/relationship_app/apps.py
+++ django-models/LibraryProject/relationship_app/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class RelationshipAppConfig(AppConfig):
     name = 'relationship_app'
+
+    def ready(self):
+        import relationship_app.models  # Ensure signals are loaded
