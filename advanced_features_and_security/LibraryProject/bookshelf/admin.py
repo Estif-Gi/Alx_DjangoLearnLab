a6ea173f20fa5c653397a037434b310b653901e5
GebetaEst <estifanosgirma4@gmail.com>
2026-01-28 13:15:49 +0300

    h

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.utils.translation import gettext_lazy as _
-
-from .models import Book, CustomUser
+from .models import Book
 
 # Register your models here.
 
@@ -12,36 +9,11 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('publication_year',)
     search_fields = ('title', 'author')
 
-@admin.register(CustomUser)
+
 class CustomUserAdmin(BaseUserAdmin):
-    # The forms to add and change user instances
-    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'date_joined')
-    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
-    search_fields = ('email', 'first_name', 'last_name')
-    ordering = ('email',)
-    
-    fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        (_('Personal info'), {
-            'fields': ('first_name', 'last_name', 'date_of_birth', 'profile_photo')
-        }),
-        (_('Permissions'), {
-            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
-        }),
-        (_('Important dates'), {
-            'fields': ('last_login', 'date_joined')
-        }),
-    )
-    
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active'),
-        }),
-    )
-    
-    # Make sure these fields are read-only for non-superusers
-    def get_readonly_fields(self, request, obj=None):
-        if not request.user.is_superuser:
-            return ('is_staff', 'is_superuser', 'groups', 'user_permissions')
-        return super().get_readonly_fields(request, obj)
+    # [keep all the existing CustomUserAdmin code]
+    ...
+
+# Add these lines at the bottom of the file:
+admin.site.register(Book, BookAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
